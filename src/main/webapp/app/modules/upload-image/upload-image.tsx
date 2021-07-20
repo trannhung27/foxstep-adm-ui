486425a900793d9a996198d2942299a76a9367ba
kienhst3 <kienhst3@fpt.com.vn>
2021-07-20 17:38:24 +0700

    update preview image

--- src/main/webapp/app/modules/upload-image/upload-image.tsx
+++ src/main/webapp/app/modules/upload-image/upload-image.tsx
@@ -20,6 +20,7 @@ export const UploadImageInput = (props: IUploadImage) => {
   const { entity, loading } = props;
   const [selectedFile, setSelectedFile] = useState(null);
   const [base64File, setBase64File] = useState('');
+  const [img, setImg] = useState('');
   const uploadImageToServer = (base64Image: string) => {
     const imageEntity = {
       content: base64Image,
@@ -29,26 +30,10 @@ export const UploadImageInput = (props: IUploadImage) => {
     props.upload(imageEntity);
   };
 
-  // const getEmergencyFoundImg = urlImg => {
-  //   const img = new Image();
-  //   img.src = urlImg;
-  //   img.crossOrigin = 'Anonymous';
-  //
-  //   const canvas = document.createElement('canvas'),
-  //     ctx = canvas.getContext('2d');
-  //
-  //   canvas.height = img.naturalHeight;
-  //   canvas.width = img.naturalWidth;
-  //   ctx.drawImage(img, 0, 0);
-  //
-  //   const b64 = canvas.toDataURL('image/png').replace(/^data:image.+;base64,/, '');
-  //   return b64;
-  // };
-
   const handleChangeImage = event => {
     setSelectedFile(event.target.files[0]);
     const reader = new FileReader();
-    reader.readAsDataURL(selectedFile);
+    reader.readAsDataURL(event.target.files[0]);
     reader.onload = function () {
       // uploadImageToServer(btoa(reader.result.toString()));
       setBase64File(reader.result.toString());
@@ -59,20 +44,16 @@ export const UploadImageInput = (props: IUploadImage) => {
     };
   };
 
-  const getBase64 = file => {
-    const baseURL = '';
-    // Make new FileReader
-    const reader = new FileReader();
+  useEffect(() => {}, [base64File]);
 
-    // Convert the file to base64 text
-    reader.readAsDataURL(file);
-  };
   const uploadHandler = () => {
     const reader = new FileReader();
     reader.readAsDataURL(selectedFile);
     reader.onload = function () {
-      uploadImageToServer(btoa(reader.result.toString()));
-      setBase64File(btoa(reader.result.toString()));
+      const imageString = reader.result.toString();
+      const imageBase64 = imageString.slice(23, imageString.length);
+      uploadImageToServer(imageBase64);
+      // setBase64File(btoa(reader.result.toString()));
     };
     reader.onerror = function () {
       // eslint-disable-next-line no-console
@@ -92,7 +73,8 @@ export const UploadImageInput = (props: IUploadImage) => {
           <Col xs="12" sm="7">
             <Label>Ảnh đại diện TT:</Label>
             <AvInput type="file" name="file" className="upload-file" id="file" onChange={handleChangeImage} required />
-            <img src={`${base64File}`} />
+
+            <img style={{ width: '40px', height: '40px' }} src={base64File} />
           </Col>
           <Col xs="12" sm="5">
             <Button color="info" onClick={uploadHandler}>
