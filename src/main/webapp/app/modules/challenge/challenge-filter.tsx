574e02384881db23744e0798b8e358913d459aa4
giangcm <giangcm@fpt.com.vn>
2021-08-06 18:48:05 +0700

    remove unused imports

--- src/main/webapp/app/modules/challenge/challenge-filter.tsx
+++ src/main/webapp/app/modules/challenge/challenge-filter.tsx
@@ -5,8 +5,6 @@ import { convertDateFromServer, convertDateTimeFromServer } from 'app/shared/uti
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ChallengeStatuses, NEWS_STATUSES } from 'app/config/constants';
 import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
-import DateTime from 'react-datetime';
-import moment from 'moment';
 
 export interface IChallengeFilterFormProps {
   challengeCriteria: Record<string, unknown>;
