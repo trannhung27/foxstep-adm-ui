fc48697777e92741a7adcaa37ae59757f20a26c7
kienhst3 <kienhst3@fpt.com.vn>
2021-08-17 09:41:15 +0700

    refactor convert function, remove unecessary import

--- src/main/webapp/app/modules/upload-image/upload-image.tsx
+++ src/main/webapp/app/modules/upload-image/upload-image.tsx
@@ -7,7 +7,7 @@ import { AvGroup, AvField } from 'availity-reactstrap-validation';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { faUpload, faFileImage } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import toImg from 'react-svg-to-image';
+import { convert } from 'app/shared/util/editor-utils';
 
 export interface IUploadImage extends StateProps, DispatchProps {
   label: string;
@@ -49,30 +49,6 @@ export const UploadImageInput = (props: IUploadImage) => {
     reader.onload = function () {
       setBase64File(reader.result.toString());
     };
-  };
-
-  const convert = src => {
-    const img = new Image();
-    img.src = src;
-
-    const canvas = convertImageToCanvas(img);
-    const pngImage = convertCanvasToImage(canvas);
-    // console.log(pngImage.src);
-    return pngImage.src;
-  };
-
-  const convertImageToCanvas = image => {
-    const canvas = document.createElement('canvas');
-    canvas.width = 300;
-    canvas.height = 240;
-    canvas.getContext('2d').drawImage(image, 0, 0, 300, 240);
-    return canvas;
-  };
-
-  const convertCanvasToImage = canvas => {
-    const image = new Image();
-    image.src = canvas.toDataURL('image/png');
-    return image;
   };
 
   useEffect(() => {
